cade45abf80c0354466f78b525d689520c7a248e
jinhui <jinhui01@yungengxin.com>
2025-09-25 10:18:27 +0800

    Update index.js and index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,2 @@
-import type {HtmlOptions as GfmFootnoteHtmlOptions} from 'micromark-extension-gfm-footnote'
-import type {Options as GfmStrikethroughOptions} from 'micromark-extension-gfm-strikethrough'
-import type {Options as GfmTableOptions} from '@jhuix/micromark-extension-gfm-table'
-
-export {gfm, gfmHtml} from './lib/index.js'
-
-/**
- * Configuration for `gfmHtml` of `micromark-extension-gfm`.
- */
-export interface HtmlOptions extends GfmFootnoteHtmlOptions {}
-
-/**
- * Configuration for `gfm` of `micromark-extension-gfm`.
- */
-export interface Options extends GfmStrikethroughOptions,GfmTableOptions {}
+export { gfm, gfmHtml } from './lib/index.js';
+export type { Options, HtmlOptions } from './lib/index.js';
